4c111821b837f8aec33699a0cc1553a1988b8e1f
Thortur <Thortur@hotmail.fr>
2018-06-06 16:57:11 +0200

    modif config

---
+++ config/netfocus.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+    'host'         => 'tiger.misterbooking.lan',
+    'port'         => 3306,
+    'driver'       => 'mysql',
+    'database'     => 'test',
+    'user'         => 'dev_christophe',
+    'password'     => 'mlpsdxc6952dgr',
+    // 'typeClass'    => 'MysqliDataBase',
+    // 'typeClass'    => 'MysqlDataBase',
+    'typeClass'    => 'PDODataBase',
+    'showMsgError' => true,
+);
